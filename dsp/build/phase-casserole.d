f8510b73129bd015895daa3072a2ed6c5a45e23a
ashgray0 <ashgray0@gmail.com>
2021-02-05 23:13:38 -0500

    Add files via upload

---
+++ dsp/build/phase-casserole.d
@@ -0,0 +1,165 @@
+build/phase-casserole.o: phase-casserole.cpp \
+ ../../libdaisy/src/daisy_pod.h ../../libdaisy/src/daisy_seed.h \
+ ../../libdaisy/src/daisy.h ../../libdaisy/src/daisy_core.h \
+ ../../libdaisy/src/sys/system.h ../../libdaisy/src/per/tim.h \
+ ../../libdaisy/src/per/qspi.h ../../libdaisy/src/per/dac.h \
+ ../../libdaisy/src/per/gpio.h ../../libdaisy/src/dev/leddriver.h \
+ ../../libdaisy/src/per/i2c.h ../../libdaisy/src/dev/sdram.h \
+ ../../libdaisy/src/dev/sr_4021.h ../../libdaisy/src/sys/system.h \
+ ../../libdaisy/src/hid/audio.h ../../libdaisy/src/per/sai.h \
+ ../../libdaisy/src/util/unique_id.h ../../libdaisy/src/per/adc.h \
+ ../../libdaisy/src/per/uart.h ../../libdaisy/src/hid/midi.h \
+ ../../libdaisy/src/util/ringbuffer.h ../../libdaisy/src/hid/encoder.h \
+ ../../libdaisy/src/hid/switch.h ../../libdaisy/src/hid/switch3.h \
+ ../../libdaisy/src/hid/ctrl.h ../../libdaisy/src/hid/gatein.h \
+ ../../libdaisy/src/hid/parameter.h ../../libdaisy/src/hid/usb.h \
+ ../../libdaisy/src/hid/logger.h ../../libdaisy/src/hid/logger_impl.h \
+ ../../libdaisy/src/per/sdmmc.h ../../libdaisy/src/per/spi.h \
+ ../../libdaisy/src/hid/oled_display.h \
+ ../../libdaisy/src/util/oled_fonts.h ../../libdaisy/src/hid/wavplayer.h \
+ ../../libdaisy/src/util/wav_format.h ../../libdaisy/src/hid/led.h \
+ ../../libdaisy/src/hid/rgb_led.h ../../libdaisy/src/util/color.h \
+ ../../libdaisy/src/dev/sr_595.h ../../libdaisy/src/dev/codec_pcm3060.h \
+ ../../libdaisy/src/dev/codec_wm8731.h \
+ ../../libdaisy/src/util/scopedirqblocker.h \
+ ../../libdaisy/Drivers/CMSIS/Include/cmsis_gcc.h ../../DaisySP/daisysp.h \
+ ../../DaisySP/modules/adenv.h ../../DaisySP/modules/adsr.h \
+ ../../DaisySP/modules/allpass.h ../../DaisySP/modules/analogbassdrum.h \
+ ../../DaisySP/modules/oscillator.h ../../DaisySP/modules/dsp.h \
+ ../../DaisySP/modules/svf.h ../../DaisySP/modules/analogsnaredrum.h \
+ ../../DaisySP/modules/atone.h ../../DaisySP/modules/autowah.h \
+ ../../DaisySP/modules/balance.h ../../DaisySP/modules/biquad.h \
+ ../../DaisySP/modules/blosc.h ../../DaisySP/modules/comb.h \
+ ../../DaisySP/modules/bitcrush.h ../../DaisySP/modules/clockednoise.h \
+ ../../DaisySP/modules/compressor.h ../../DaisySP/modules/crossfade.h \
+ ../../DaisySP/modules/dcblock.h ../../DaisySP/modules/decimator.h \
+ ../../DaisySP/modules/delayline.h ../../DaisySP/modules/drip.h \
+ ../../DaisySP/modules/dust.h ../../DaisySP/modules/fm2.h \
+ ../../DaisySP/modules/fold.h ../../DaisySP/modules/formantosc.h \
+ ../../DaisySP/modules/fractal_noise.h ../../DaisySP/modules/grainlet.h \
+ ../../DaisySP/modules/harmonic_osc.h ../../DaisySP/modules/hihat.h \
+ ../../DaisySP/modules/jitter.h ../../DaisySP/modules/limiter.h \
+ ../../DaisySP/modules/line.h ../../DaisySP/modules/maytrig.h \
+ ../../DaisySP/modules/metro.h ../../DaisySP/modules/modalvoice.h \
+ ../../DaisySP/modules/resonator.h ../../DaisySP/modules/mode.h \
+ ../../DaisySP/modules/moogladder.h ../../DaisySP/modules/nlfilt.h \
+ ../../DaisySP/modules/oscillatorbank.h ../../DaisySP/modules/overdrive.h \
+ ../../DaisySP/modules/particle.h ../../DaisySP/modules/pd.h \
+ ../../DaisySP/modules/phasor.h ../../DaisySP/modules/port.h \
+ ../../DaisySP/modules/pitchshifter.h ../../DaisySP/modules/PolyPluck.h \
+ ../../DaisySP/modules/pluck.h ../../DaisySP/modules/reverbsc.h \
+ ../../DaisySP/modules/samplehold.h \
+ ../../DaisySP/modules/sampleratereducer.h \
+ ../../DaisySP/modules/smooth_random.h ../../DaisySP/modules/string.h \
+ ../../DaisySP/modules/tone.h ../../DaisySP/modules/synthbassdrum.h \
+ ../../DaisySP/modules/synthsnaredrum.h \
+ ../../DaisySP/modules/variablesawosc.h \
+ ../../DaisySP/modules/variableshapeosc.h ../../DaisySP/modules/vosim.h \
+ ../../DaisySP/modules/whitenoise.h ../../DaisySP/modules/zoscillator.h
+../../libdaisy/src/daisy_pod.h:
+../../libdaisy/src/daisy_seed.h:
+../../libdaisy/src/daisy.h:
+../../libdaisy/src/daisy_core.h:
+../../libdaisy/src/sys/system.h:
+../../libdaisy/src/per/tim.h:
+../../libdaisy/src/per/qspi.h:
+../../libdaisy/src/per/dac.h:
+../../libdaisy/src/per/gpio.h:
+../../libdaisy/src/dev/leddriver.h:
+../../libdaisy/src/per/i2c.h:
+../../libdaisy/src/dev/sdram.h:
+../../libdaisy/src/dev/sr_4021.h:
+../../libdaisy/src/sys/system.h:
+../../libdaisy/src/hid/audio.h:
+../../libdaisy/src/per/sai.h:
+../../libdaisy/src/util/unique_id.h:
+../../libdaisy/src/per/adc.h:
+../../libdaisy/src/per/uart.h:
+../../libdaisy/src/hid/midi.h:
+../../libdaisy/src/util/ringbuffer.h:
+../../libdaisy/src/hid/encoder.h:
+../../libdaisy/src/hid/switch.h:
+../../libdaisy/src/hid/switch3.h:
+../../libdaisy/src/hid/ctrl.h:
+../../libdaisy/src/hid/gatein.h:
+../../libdaisy/src/hid/parameter.h:
+../../libdaisy/src/hid/usb.h:
+../../libdaisy/src/hid/logger.h:
+../../libdaisy/src/hid/logger_impl.h:
+../../libdaisy/src/per/sdmmc.h:
+../../libdaisy/src/per/spi.h:
+../../libdaisy/src/hid/oled_display.h:
+../../libdaisy/src/util/oled_fonts.h:
+../../libdaisy/src/hid/wavplayer.h:
+../../libdaisy/src/util/wav_format.h:
+../../libdaisy/src/hid/led.h:
+../../libdaisy/src/hid/rgb_led.h:
+../../libdaisy/src/util/color.h:
+../../libdaisy/src/dev/sr_595.h:
+../../libdaisy/src/dev/codec_pcm3060.h:
+../../libdaisy/src/dev/codec_wm8731.h:
+../../libdaisy/src/util/scopedirqblocker.h:
+../../libdaisy/Drivers/CMSIS/Include/cmsis_gcc.h:
+../../DaisySP/daisysp.h:
+../../DaisySP/modules/adenv.h:
+../../DaisySP/modules/adsr.h:
+../../DaisySP/modules/allpass.h:
+../../DaisySP/modules/analogbassdrum.h:
+../../DaisySP/modules/oscillator.h:
+../../DaisySP/modules/dsp.h:
+../../DaisySP/modules/svf.h:
+../../DaisySP/modules/analogsnaredrum.h:
+../../DaisySP/modules/atone.h:
+../../DaisySP/modules/autowah.h:
+../../DaisySP/modules/balance.h:
+../../DaisySP/modules/biquad.h:
+../../DaisySP/modules/blosc.h:
+../../DaisySP/modules/comb.h:
+../../DaisySP/modules/bitcrush.h:
+../../DaisySP/modules/clockednoise.h:
+../../DaisySP/modules/compressor.h:
+../../DaisySP/modules/crossfade.h:
+../../DaisySP/modules/dcblock.h:
+../../DaisySP/modules/decimator.h:
+../../DaisySP/modules/delayline.h:
+../../DaisySP/modules/drip.h:
+../../DaisySP/modules/dust.h:
+../../DaisySP/modules/fm2.h:
+../../DaisySP/modules/fold.h:
+../../DaisySP/modules/formantosc.h:
+../../DaisySP/modules/fractal_noise.h:
+../../DaisySP/modules/grainlet.h:
+../../DaisySP/modules/harmonic_osc.h:
+../../DaisySP/modules/hihat.h:
+../../DaisySP/modules/jitter.h:
+../../DaisySP/modules/limiter.h:
+../../DaisySP/modules/line.h:
+../../DaisySP/modules/maytrig.h:
+../../DaisySP/modules/metro.h:
+../../DaisySP/modules/modalvoice.h:
+../../DaisySP/modules/resonator.h:
+../../DaisySP/modules/mode.h:
+../../DaisySP/modules/moogladder.h:
+../../DaisySP/modules/nlfilt.h:
+../../DaisySP/modules/oscillatorbank.h:
+../../DaisySP/modules/overdrive.h:
+../../DaisySP/modules/particle.h:
+../../DaisySP/modules/pd.h:
+../../DaisySP/modules/phasor.h:
+../../DaisySP/modules/port.h:
+../../DaisySP/modules/pitchshifter.h:
+../../DaisySP/modules/PolyPluck.h:
+../../DaisySP/modules/pluck.h:
+../../DaisySP/modules/reverbsc.h:
+../../DaisySP/modules/samplehold.h:
+../../DaisySP/modules/sampleratereducer.h:
+../../DaisySP/modules/smooth_random.h:
+../../DaisySP/modules/string.h:
+../../DaisySP/modules/tone.h:
+../../DaisySP/modules/synthbassdrum.h:
+../../DaisySP/modules/synthsnaredrum.h:
+../../DaisySP/modules/variablesawosc.h:
+../../DaisySP/modules/variableshapeosc.h:
+../../DaisySP/modules/vosim.h:
+../../DaisySP/modules/whitenoise.h:
+../../DaisySP/modules/zoscillator.h:
